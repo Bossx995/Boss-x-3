707e2850bff24520595ad2e741d010ccc658bc0e
Boss <bm20087267@gmail.com>
2026-09-09 10:24:23 +0530

    Add settings configuration for Boss x Bot

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Boss x Bot',
+  author: 'bikram‎',
+  botName: "Boss x Bot",
+  botOwner: 'Bikram', // Your name
+  ownerNumber: '919123827995', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/Bossx995/Boss-x.git",
+};
+
+module.exports = settings;
